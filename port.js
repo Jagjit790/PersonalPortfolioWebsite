aa75a2d8a2c99fb1e0e2dcc3e19ad25ea74d25db
Jagjit790 <singhjagjit3914@gmail.com>
2026-07-11 17:35:01 +0530

    Delete port.js

--- port.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  // Set delay in milliseconds (e.g., 3000ms = 3 seconds)
-  const delay = 3500;
-
-  setTimeout(function () {
-    document.getElementById("preloader").style.display = "none";
-    document.body.classList.add("loaded");
-  }, delay);
-});
